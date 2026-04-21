c1491383cc4654a5896965dc181a7c2424c3b232
karimKandil0 <kimo989yt@gmail.com>
2026-04-21 03:55:26 +0200

    zbasu: cusku'i la .main.rs. greku

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+#[panic_handler]
+fn panic_handler(_info: &core::panic::PanicInfo) -> ! {
+    loop { }
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> ! {
+    loop { }
 }
---
+++ kernel/target/x86_64-unknown-none/debug/deps/kernel-025dc2fe868022ee.d
@@ -0,0 +1,5 @@
+/home/karimkandil/K-ernel/kernel/target/x86_64-unknown-none/debug/deps/kernel-025dc2fe868022ee: src/main.rs
+
+/home/karimkandil/K-ernel/kernel/target/x86_64-unknown-none/debug/deps/kernel-025dc2fe868022ee.d: src/main.rs
+
+src/main.rs:
