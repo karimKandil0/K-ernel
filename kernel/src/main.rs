85b1aec97e281d84b6fdba109e7ce5889219fbf8
karimKandil0 <kimo989yt@gmail.com>
2026-04-21 19:36:55 +0200

    zbasu: cusku'i lo lerfu

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -4,6 +4,8 @@ use limine::request::FramebufferRequest;
 #[unsafe(link_section = ".requests")]
 static FRAMEBUFFER: FramebufferRequest = FramebufferRequest::new();
 
+static FONT: &[u8] = include_bytes!("font.psf");
+
 #[panic_handler]
 fn panic_handler(_info: &core::panic::PanicInfo) -> ! {
     loop { }
@@ -13,20 +15,35 @@ fn panic_handler(_info: &core::panic::PanicInfo) -> ! {
 pub extern "C" fn _start() -> ! {
 
     if let Some(response) = FRAMEBUFFER.response() {
+
         if let Some(fb) = response.framebuffers().first() {
+
             let ptr = fb.address() as *mut u32;
-            unsafe {
-                for y in 0..100 {
-                    for x in 0..100 {
-                        let offset = (y * fb.pitch as usize + x * (fb.bpp as usize / 8)) / 4;
-                        unsafe {
-                            ptr.add(offset).write_volatile(0x0000FF00)
-                        }
-                    }
-                }
-            }
+            
+            draw_char(ptr, fb.pitch, fb.bpp, 10, 10 , b'K');
         }
     }
 
     loop { }
 }
+
+fn draw_char(ptr: *mut u32, pitch: u64, bpp: u16, x: usize, y: usize, c: u8) -> () {
+    let glyph_offset = 32 + (c as usize * 16);
+    let glyph = &FONT[glyph_offset..];
+
+    for row_idx in 0..16 {
+        let row_byte = glyph[row_idx];
+        for bit in 0..8 {
+            if row_byte & (0x80 >> bit) != 0 {
+                let px = x + bit;
+                let py = y + row_idx;
+                let offset = (py * pitch as usize + px * (bpp as usize / 8)) / 4;
+                unsafe {
+                    ptr.add(offset).write_volatile(0x00FF0000);
+                }
+           }
+        }
+    }
+    
+}
+
