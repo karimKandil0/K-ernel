a2ff4c367a65220582a3dc4bf7d531a0c2f0e3d4
karimKandil0 <kimo989yt@gmail.com>
2026-04-21 04:14:40 +0200

    zbasu: la .main.rs. cu ciska la .vga buffer.

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -8,5 +8,12 @@ fn panic_handler(_info: &core::panic::PanicInfo) -> ! {
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
+    let vga_buf: *mut u8 = 0xB8000 as *mut u8;
+
+    unsafe {
+        vga_buf.write_volatile(b'K');
+        vga_buf.add(1).write_volatile(0x0F);
+    }
+
     loop { }
 }
